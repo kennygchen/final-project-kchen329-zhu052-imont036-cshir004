30cafeea9d8c8cfa35f15d04f57cd9560737cc18
kennygchen <kennygchen@yahoo.com>
2021-12-01 01:08:40 -0800

    Added WarriorFactory and RogueFactory tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 #include "warrior_character_test.hpp"
 #include "rogue_character_test.hpp"
+#include "warrior_factory_test.hpp"
+#include "rogue_factory_test.hpp"
 
 int main(int argc, char **argv){
 	::testing::InitGoogleTest(&argc, argv);
