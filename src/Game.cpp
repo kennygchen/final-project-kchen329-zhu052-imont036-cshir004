f52a866db8549466ca3b9d399364a2d35dc2664d
ColeShirakata <coleshirakata@gmail.com>
2021-12-06 08:01:11 -0800

    Made some minor changes to the shop

--- src/Game.cpp
+++ src/Game.cpp
@@ -51,22 +51,21 @@ void Game::shop() {
 	do {
 		cout << endl;
 		cout << "=====WELCOME TO THE SHOP=====" << endl;
+		cout << "Your Gold: " << character->getGold() << endl;
+		cout << endl;
 		cout << "(WEAPONS)" << endl;
-		cout << "1) Dagger --- 10 gold" << endl;
-		cout << "2) Axe --- 100 gold" << endl;
-		cout << "3) Sword --- 1000 gold" << endl;
+		cout << "1) Sword --- 10 gold" << endl;
 		cout << "(POTIONS)" << endl;
-		cout << "4) Health Potion --- 50 gold" << endl;
-		cout << "(ARMOR)" << endl;
-		cout << "5) Helmet --- 100 gold" << endl;
-		cout << "6) Chestplate --- 1000 gold" << endl;
-		cout << "7) Leave shop & enter dungeon" << endl;
+		cout << "2) Health Potion --- 50 gold" << endl;
+		cout << endl;
+		cout << "3) Leave shop & enter dungeon" << endl;
 		cin >> input;
+		cout << endl;
 
 		switch(input) {
 			case 1:
 				if(character->getGold() >= 10) {
-					cout << "You purchased a dagger" << endl;
+					cout << "You purchased a sword" << endl;
 					character->setDamage(10);
 					character->setGold(character->getGold()-10);
 				}
@@ -75,27 +74,7 @@ void Game::shop() {
 				}
 				break;	
 			case 2:
-				if(character->getGold() >= 100) {
-					cout << "You purchased an axe" << endl;
-					character->setDamage(15);
-					character->setGold(character->getGold()-100);
-				}
-				else {
-					cout << "You don't have enough gold." << endl;
-				}
-				break;
-			case 3:
-				if(character->getGold() >= 1000) {
-					cout << "You purchased a sword" << endl;
-					character->setDamage(20);
-					character->setGold(character->getGold()-1000);
-				}
-				else {
-					cout << "You don't have enought gold. " << endl;
-				}
-				break;
-			case 4:
-				if(character->getGold() >= 50) {
+					if(character->getGold() >= 50) {
 					cout << "You purhcased a health potion." << endl;
 					character->setGold(character->getGold()-50);
 				}
@@ -103,29 +82,11 @@ void Game::shop() {
 					cout << "You don't have enough gold." << endl;
 				}
 				break;
-			case 5:
-				if(character->getGold() >= 100) {
-					cout << "You purchased a helmet" << endl;
-					character->setGold(character->getGold()-100);
-				}
-				else {
-					cout << "You don't have enough gold." << endl;
-				}
-				break;
-			case 6:
-				if(character->getGold() >= 1000) {
-					cout << "You purchased a chestplate." << endl;
-					character->setGold(character->getGold()-1000);
-				}
-				else {
-					cout << "You don't have enough gold." << endl;
-				}
-				break;
-			case 7:
+			case 3:
 				cout << "You exit the shop & make your way towards the dungeon!" << endl;
 				break;
 		}
-	} while(input < 7);
+	} while(input < 3);
 }
 
 int Game::getHealth(){
--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,6 @@ using std::cout;
 using std::endl;
 using std::string;
 
-void shop() {
-	cout << "-----Welcome to the shop-----" << endl;
-	cout << "1) Dagger --- 100 gold" << endl;
-	cout << "2) Sword --- 200 gold" << endl;
-	cout << "3) Leave shop " << endl;
-	cout << "What would you like to purchase? " << endl;
-	
-	int input;
-	cin >> input;
-	
-	if (input == 1) {
-		cout << "You have purchased the dagger." << endl;
-	}
-	else if (input == 2) {
-		cout << "You have purchased the sword." << endl;
-	}
-	else {
-		cout << "You exit the shop" << endl;
-	}
-}
-
 int main() {
 	int input;
 	cout << "Choose a class: " << endl;
